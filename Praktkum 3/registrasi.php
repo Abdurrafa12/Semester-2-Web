a95120102f5fd9167118512dcd46660130bed9df
Aabdurrafa <146910730+Abdurrafa12@users.noreply.github.com>
2024-03-18 10:31:14 +0700

    Add files via upload

---
+++ Praktkum 3/registrasi.php
@@ -0,0 +1,8 @@
+<?php
+$domisili = ["jakarta","bogor","depok","tanggerang","bekasi"];
+
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];
